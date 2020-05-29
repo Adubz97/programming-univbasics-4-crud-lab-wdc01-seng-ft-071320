37316c2aa2183f7504b61a8b67c16cde1d89072c
Abdullah Albasha <abdullah.a.albasha@gmail.com>
2020-05-29 04:16:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,30 +7,27 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
   
   
 end
 
 def add_element_to_start_of_array(array, element)
-  car_dealer =["Ferrari", "Lamborghini", "Mercedes" , "Tesla","Porsche"]
-  car_dealer.unshift("Bentley")
+  array.unshift(element)
   
 end
 
 def remove_element_from_end_of_array(array)
-  car_dealer = ["Ferrari", "Lamborghini", "Mercedes" , "Tesla"]
-  car_dealer.pop("Porsche")
+  array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
-  car_dealer = ["Ferrari", "Lamborghini", "Mercedes" , "Tesla"]
-  car_dealer.shift("Bentley")
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array.
 end
 
 def retrieve_first_element_from_array(array)
