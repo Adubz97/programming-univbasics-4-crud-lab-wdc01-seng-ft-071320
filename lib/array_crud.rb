51b8df3fc2a456141b6259359323662aa30de27a
Abdullah Albasha <abdullah.a.albasha@gmail.com>
2020-05-29 04:20:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,18 +8,14 @@ end
 
 def add_element_to_end_of_array(array, element)
   array.push(element)
-  
-  
 end
 
 def add_element_to_start_of_array(array, element)
   array.unshift(element)
-  
 end
 
 def remove_element_from_end_of_array(array)
   array.pop
-  
 end
 
 def remove_element_from_start_of_array(array)
@@ -27,17 +23,17 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.
+  array[]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array[]
 end
