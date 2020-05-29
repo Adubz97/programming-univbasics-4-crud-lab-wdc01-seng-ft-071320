57ff8ac0c00797fd303d0214c76ac253a6067971
Abdullah Albasha <abdullah.a.albasha@gmail.com>
2020-05-29 03:46:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   car_dealer = ["Ferrari", "Lamborghini", "Mercedes" , "Tesla"]
-  car_dealer.push="Porsche"
-end
+  car_dealer.push = "porsche"
 
 def add_element_to_start_of_array(array, element)
   car_dealer =["Ferrari", "Lamborghini", "Mercedes" , "Tesla","Porsche"]
